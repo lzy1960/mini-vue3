feab4d488e25d572130aa221846bca22f529e05e
lzy1960 <1960553899@qq.com>
2022-05-24 21:55:12 +0800

    reactive、effect、scheduler

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,52 @@
+class ReactiveEffect {
+  private _fn
+
+  constructor(fn, public scheduler?) {
+    this._fn = fn
+  }
+
+  run () {
+    activeEffect = this
+    return this._fn()
+  }
+}
+
+const targetMap = new Map()
+export const track = (target, key) => {
+  // target -> key -> dep
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+
+  let dep = depsMap.get(key)
+  if (!dep) {
+    dep = new Set()
+    depsMap.set(key, dep)
+  }
+
+  dep.add(activeEffect)
+}
+
+export const trigger = (target, key) => {
+  let depsMap = targetMap.get(target)
+  let dep = depsMap.get(key)
+
+  for (const effect of dep) {
+    if (effect.scheduler) {
+      effect.scheduler()
+    } else {
+      effect.run()
+    }
+  }
+}
+
+let activeEffect
+export const effect = (fn, options = {}) => {
+  const _effect = new ReactiveEffect(fn, options.scheduler)
+
+  _effect.run()
+
+  return _effect.run.bind(_effect)
+}
---
+++ src/reactivity/tests/effect.spec.ts
@@ -0,0 +1,57 @@
+import { reactive } from '../reactive';
+import { effect } from '../effect';
+
+describe('effect', () => {
+  it('happy path', () => {
+    const user = reactive({
+      age: 10
+    })
+
+    let nextAge
+    effect(() => {
+      nextAge = user.age + 1
+    })
+    expect(nextAge).toBe(11)
+
+    user.age++
+    expect(nextAge).toBe(12)
+  });
+
+  it('should return runner when call runner', () => {
+    // 1. effect(fn) -> function (runner) -> fn -> return
+    let foo = 10
+    const runner = effect(() => {
+      foo++
+      return 'foo'
+    })
+
+    expect(foo).toBe(11)
+
+    const r = runner()
+    expect(foo).toBe(12)
+    expect(r).toBe('foo')
+  });
+  it('scheduler', () => {
+    let dummy
+    let run
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+    const obj = reactive({ foo: 1 })
+    const runner = effect(() => {
+      dummy = obj.foo
+    },
+      { scheduler }
+    )
+
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    expect(dummy).toBe(1)
+
+    run()
+    expect(dummy).toBe(2)
+  })
+});
