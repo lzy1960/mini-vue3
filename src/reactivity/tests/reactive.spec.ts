8c59f597448b90aa23bb101181de7e3b99a7512c
lzy1960 <1960553899@qq.com>
2022-07-13 14:56:13 +0800

    test: 增加toRaw测试用例

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -34,4 +34,24 @@ describe('reactive', () => {
     expect(isReactive(observed)).toBe(true)
     expect(isReactive(observed.foo)).toBe(false)
   })
+  it('happy path', () => {
+    // toRaw 可以 return 通过 `reactive` 、 `readonly` 、`shallowReactive` 、`shallowReadonly` 包装的 origin 值
+    const reactiveOrigin = { key: 'reactive' }
+    expect(toRaw(reactive(reactiveOrigin))).toEqual(reactiveOrigin)
+    const readonlyOrigin = { key: 'readonly' }
+    expect(toRaw(readonly(readonlyOrigin))).toEqual(readonlyOrigin)
+    const shallowReadonlyOrigin = { key: 'shallowReadonly' }
+    expect(toRaw(shallowReadonly(shallowReadonlyOrigin))).toEqual(
+      shallowReadonlyOrigin
+    )
+    const shallowReactiveOrigin = { key: 'shallowReactive' }
+    expect(toRaw(shallowReactive(shallowReactiveOrigin))).toEqual(
+      shallowReactiveOrigin
+    )
+
+    const nestedWrapped = {
+      foo: { bar: { baz: 1 }, foo2: { bar: { baz: 2 } } },
+    }
+    expect(toRaw(reactive(nestedWrapped))).toEqual(nestedWrapped)
+  })
 });
